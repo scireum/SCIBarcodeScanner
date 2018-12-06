fa234f217e17fdd3fa2444dad11b554cd4ad015e
Jakob Vogel <jvo@scireum.de>
2018-12-06 23:33:12 +0100

    Enables toggling of torch

--- SCIBarcodeScannerExample/ViewController.swift
+++ SCIBarcodeScannerExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, SCIBarcodeScannerViewDelegate {
     }
 
     @IBAction func toggleTorch(_ sender: Any?) {
+        self.scannerFrame.toggleTorch()
     }
 
     func sciBarcodeScannerViewReceived(code: String, type: String) {
