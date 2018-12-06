191c4362c4050182ced4e9c0ebc96370524d7308
Jakob Vogel <jvo@scireum.de>
2018-12-06 21:59:07 +0100

    Adds torch toggling button

--- SCIBarcodeScannerExample/ViewController.swift
+++ SCIBarcodeScannerExample/ViewController.swift
@@ -4,10 +4,16 @@ import SCIBarcodeScanner
 class ViewController: UIViewController, SCIBarcodeScannerViewDelegate {
 
     @IBOutlet var scannerFrame: SCIBarcodeScannerView!
+    @IBOutlet var torchButton: UIBarButtonItem!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.scannerFrame.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func toggleTorch(_ sender: Any?) {
     }
 
     func sciBarcodeScannerViewReceived(code: String, type: String) {
